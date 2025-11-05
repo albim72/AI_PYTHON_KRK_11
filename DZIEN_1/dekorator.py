4d3619f474ab3e9e6bc53a34da87a7a5537959f3
albim72 <marcin.albiniak72@gmail.com>
2025-11-05 12:31:23 +0100

    Update dekorator.py

--- DZIEN_1/dekorator.py
+++ DZIEN_1/dekorator.py
@@ -12,3 +12,25 @@ def przywitaj():
     print("Cześć!!!")
 
 przywitaj()
+
+#dekorator pomiaru czasu
+import time
+
+def czas(funkcja):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        wynik = funkcja(*args, **kwargs)
+        stop = time.time()
+        print(f"{funkcja.__name__}: {stop-start}")
+        return wynik
+    return wrapper
+
+
+@czas
+def oblicz():
+    suma=0
+    for i in range(20_000_000):
+        suma += i
+    return suma
+
+oblicz()
